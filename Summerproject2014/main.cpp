3d9b65747c3372cc0361d20fbb3035936576be6c
perkulatorn <wolfen92@hotmail.com>
2014-06-17 22:43:30 +0200

    w25.1

---
+++ Summerproject2014/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "SFML\Graphics.hpp"
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(100, 100), "ASDF");
+	std::cout << "TEST råka stänga ner din arkanoid 4 mapp.." << std::endl;
+	while (window.isOpen())
+	{
+	
+	}
+	return 0;
+}
